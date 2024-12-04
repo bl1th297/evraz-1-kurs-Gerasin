b17e2194b5f599236a361bd34a2b5648031315d3
bl1th <blth95220@gmail.com>
2024-12-04 19:57:28 +0500

    условия 2 задача.js

---
+++ 2 задача условия.js
@@ -0,0 +1,9 @@
+document.getElementById('input').value;
+function checkInput(){
+    if (input$2 !== 0 );
+    document.getElementById (result().innerText = 'Введённое число чётное'
+}
+else{
+    document.getElementById(result().innerText = 'Введённое число нечётное'
+
+}
--- условия 1 задача.js
+++ условия 1 задача.js
@@ -1,2 +1,82 @@
-function makeBlack () }
-let
+let number = 0
+function function1() {
+    number = 0
+    if (number == '0'){
+        MakeBlack()
+    }
+}
+function function2() {
+    number = 1
+    if (number == '1') {
+        MakeWhite()
+    }
+}
+function function3() {
+    number = 2
+    if (number == '2') {
+        MakeBlue()
+    }
+}
+function function4() {
+    number = 5
+    if (number == '5') {
+        MakeRed()
+    }
+}
+function function5() {
+    number = number + 1
+    if (number !== '0') {
+        MakeRed();
+    }
+    else{
+        MakeGold();
+    }
+}
+function function6() {
+    number = number - 1
+    if (number !== '0') {
+        MakeGold();
+    }
+        else{
+        MakeRed();
+    }
+}
+function MakeBlack(){
+    backgroundcolor = 'black';
+    let element = document.getElementById("body");
+    element.style.backgroundColor = "black"; // Изменяем цвет фона
+    element.style.color = "white"; // Изменяем цвет текста
+}
+function MakeWhite(){
+    backgroundcolor = 'white';
+    let element = document.getElementById("body");
+    element.style.backgroundColor = "white"; // Изменяем цвет фона
+    element.style.color = "black"; // Изменяем цвет текста
+}
+function MakeBlue(){
+    backgroundcolor = 'blue';
+    let  element = document.getElementById("body");
+    element.style.backgroundColor = "blue"; // Изменяем цвет фона
+    element.style.color = "white"; // Изменяем цвет текста
+}
+function MakeGreen(){
+    backgroundcolor = 'green';
+    let  element = document.getElementById("body");
+    element.style.backgroundColor = "green"; // Изменяем цвет фона
+    element.style.color = "white"; // Изменяем цвет текста
+}
+function MakeRed(){
+    backgroundcolor = 'red';
+    let  element = document.getElementById("body");
+    element.style.backgroundColor = "red"; // Изменяем цвет фона
+    element.style.color = "black"; // Изменяем цвет текста
+}
+function MakeGold() {
+    backgroundcolor = 'gold';
+    let element = document.getElementById("body");
+    element.style.backgroundColor = "gold"; // Изменяем цвет фона
+    element.style.color = "black"; // Изменяем цвет текста
+}
+function innerText (){
+
+}
