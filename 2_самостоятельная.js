2d9d1fde3d6f81ea7260bd488453fb050985d10f
bl1th <blth95220@gmail.com>
2024-12-25 20:00:20 +0500

    2 полугодие

--- 2_самостоятельная.js
+++ 2_самостоятельная.js
@@ -1,76 +1,67 @@
-function MakeWhite(){
-    let element = document.getElementById("body");
-    element.style.backgroundColor = "black"; // Изменяем цвет фона
-    element.style.color = "white"; // Изменяем цвет текста
+let a = 0;
+let sp = document.getElementById('total');
+let chetnoe = document.getElementById('even');
+let three = document.getElementById('three');
+let five = document.getElementById('five');
+let seven = document.getElementById('seven');
+let history_even = document.getElementById("history_even")
+let history_three = document.getElementById("history_three")
+let history_five = document.getElementById("history_five")
+let history_seven = document.getElementById("history_seven")
+let history = document.getElementById("history")
+
+function minus10() {
+    a = a - 10;
+    // возможен короткий вариант a -= 10;
+    sp.innerText = a;
+
 }
 
-function MakeBlack() {
-    let element = document.getElementById("body");
-    element.style.backgroundColor = "white"; // Изменяем цвет фона
-    element.style.color = "black"; // Изменяем цвет текста
-}
-function MakeGreen() {
-    let element = document.getElementById("body");
-    element.style.backgroundColor = "green"; // Изменяем цвет фона
-    element.style.color = "black"; // Изменяем цвет текста
-}
-function MakeGold() {
-    let element = document.getElementById("body");
-    element.style.backgroundColor = "Gold"; // Изменяем цвет фона
-    element.style.color = "black"; // Изменяем цвет текста
+function minus1() {
+    a = a - 1;
+    // возможен короткий вариант a -= 1;
+    sp.innerText = a;
 }
 
-function MakeBlue() {
-    let element = document.getElementById("body");
-    element.style.backgroundColor = "blue"; // Изменяем цвет фона
-    element.style.color = "greenyellow"; // Изменяем цвет текста
-}
-let a = 0
-function min10() {
-    a = +a - 10
-    result.innerText = a
+function plus1() {
+    a = a + 1;
+    // возможен короткий вариант a += 1;
+    sp.innerText = a;
+
     if (a % 2 === 0) {
-        answer = 'Введённое число чётное'
-    } else {
-    answer = 'Введённое число нечётное'
-
-result.innerText = answer
-}
-
-
-
-
-}
-function min1() {
-    a = +a - 1
-    result.innerText = a
-
-    answer = 'Введённое число чётное'
-}
-    function plus1() {
-        a = +a + 1
-        result.innerText = a
-        if (a % 2 === 0)
-        answer = 'Введённое число чётное'
-    else{
-        {
-            answer = 'Введённое число нечётное'
-        }
-        result.innerText = answer
+        chetnoe.innerText = 'Да';
+    }
+    else {
+        chetnoe.innerText = 'Нет';
     }
 
+    if (a % 3 === 0) {
+        three.innerText = 'Да';
+    }
+    else {
+        three.innerText = 'Нет';
+    }
+
+    if (a % 5 === 0) {
+        five.innerText = 'Да';
+    }
+    else {
+        five.innerText = 'Нет';
+    }
+
+    if (a % 7 === 0) {
+        seven.innerText = 'Да';
+    }
+    else {
+        seven.innerText = 'Нет';
+    }
 }
-    function plus10() {
-    a = +a + 10
-        result.innerText = a
+
+function plus10() {
+    a = a + 10;
+    // возможен короткий вариант a += 10;
+    sp.innerText = a;
+    if (a % 2 === 0 ) {
+
     }
-    if (a % 2 === 0) {
-        answer = 'Введённое число чётное'
-    } else {
-        answer = 'Введённое число нечётное'
-        result.innerText = answer
-    }
-
-
-
-
+}
