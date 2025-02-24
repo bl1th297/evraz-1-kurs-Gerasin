203bd983753d6057b1e58b0ee1e73847e6523825
bl1th <blth95220@gmail.com>
2025-02-24 17:54:46 +0500

    новая тема (26.02.25)

--- 7maccivy.js
+++ 7maccivy.js
@@ -1,4 +1,4 @@
-let list = [];
+let list = []
 
 // функция добавления значения из инпута в массив и див
 function add2() {
@@ -14,5 +14,39 @@ function add2() {
 
 
     ol.append(liElement)
+
+}
+function add7(){
+    let input = document.getElementById('bebra');
+    let input1 =document.getElementById('aboba');
+    let ol = document.getElementById('tt');
+
+
+    if (input.value === ' ') {
+        alert('Введите значение')
+        return;
+    }
+
+    if (input1.value === ' ') {
+        alert('Ты гонишь')
+        return;
+    }
+
+    list[input.value] = input1.value;
+    ol.innerText = '';
+
+    for(let i = 0;i < list.length; i++) {
+        let liElement = document.createElement('li');
+        liElement.innerText = list[i];
+        ol.append(liElement)
+    }
+    console.log('длина массива', list.length);
+    console.log(list);
+
+
+
+
+
+
 
 }
